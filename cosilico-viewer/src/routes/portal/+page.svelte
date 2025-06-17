b2e63d8a395e0e0c9baa9c20189555011ca9d4d3
Erik Storrs <epstorrs@gmail.com>
2025-06-17 09:13:10 -0500

    working on sidebar

--- cosilico-viewer/src/routes/portal/+page.svelte
+++ cosilico-viewer/src/routes/portal/+page.svelte
@@ -5,37 +5,37 @@
   import type { PageData } from './$types.js'
 
   let { data } = $props()
-  let { notes, supabase, user } = $derived(data)
+  let { directory_entities, profile, supabase, user } = $derived(data)
 
-  const handleSubmit: EventHandler<SubmitEvent, HTMLFormElement> = async (evt) => {
-    evt.preventDefault()
-    if (!evt.target) return
+  // const handleSubmit: EventHandler<SubmitEvent, HTMLFormElement> = async (evt) => {
+  //   evt.preventDefault()
+  //   if (!evt.target) return
 
-    const form = evt.target as HTMLFormElement
+  //   const form = evt.target as HTMLFormElement
 
-    const note = (new FormData(form).get('note') ?? '') as string
-    if (!note) return
+  //   const note = (new FormData(form).get('note') ?? '') as string
+  //   if (!note) return
 
-    const { error } = await supabase.from('notes').insert({ note })
-    if (error) console.error(error)
+  //   const { error } = await supabase.from('notes').insert({ note })
+  //   if (error) console.error(error)
 
-    invalidate('supabase:db:notes')
-    form.reset()
-  }
+  //   invalidate('supabase:db:notes')
+  //   form.reset()
+  // }
 </script>
 
-<h1>Private page for user: {user?.email}</h1>
-<h2>Notes</h2>
+<h1>Directories user: {user?.email}</h1>
+<h2>Directories</h2>
 <ul>
-  {#each notes as note}
-    <li>{note.note}</li>
+  {#each directory_entities as entity}
+    <li>{entity.name}</li>
   {/each}
 </ul>
-<form onsubmit={handleSubmit}>
+<!-- <form onsubmit={handleSubmit}>
   <label>
     Add a note
     <input name="note" type="text" />
   </label>
-</form>
+</form> -->
 
 

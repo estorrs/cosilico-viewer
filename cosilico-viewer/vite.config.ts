e926c015c20f06459bec70f349bf611a2f833cf5
Erik Storrs <epstorrs@gmail.com>
2025-07-29 06:53:44 -0500

    fixing image viewing in production bug

--- cosilico-viewer/vite.config.ts
+++ cosilico-viewer/vite.config.ts
@@ -3,5 +3,17 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
-
+	build:{
+		minify:'terser',
+		terserOptions:{
+			keep_fnames:true,
+			mangle:{
+				keep_fnames:true,
+				reserved:[
+					'minMaxRangePixelTransform',
+					'applyPseudocolorToPixel'
+				]
+			}
+		}
+	}
 });
